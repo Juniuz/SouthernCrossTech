5700720bd9aeb48175bf3993ed6ea3fcc707fbf3
Junius Dimasuhid <JDimasuhid@flintfox.com>
2021-12-18 15:54:05 +1300

    add member search and result page

--- SouthernCross.Web/Controllers/MemberController.cs
+++ SouthernCross.Web/Controllers/MemberController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SouthernCross.Web.Models;
 
 namespace SouthernCross.Web.Controllers
 {
     public class MemberController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<MemberController> _logger;
+
+        public MemberController(ILogger<MemberController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult MemberSearch()
         {
             return View();
+        }
+
+        public IActionResult SearchResult()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
