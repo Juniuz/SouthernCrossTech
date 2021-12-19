33bbf31837cfb286f327094aee8a250267ed900e
Junius Dimasuhid <JDimasuhid@flintfox.com>
2021-12-20 09:24:24 +1300

    add unit test for member card number and policy number digit length

--- SouthernCrossWeb.Tests/MemberIdentificationTest.cs
+++ SouthernCrossWeb.Tests/MemberIdentificationTest.cs
@@ -1,13 +1,24 @@
+using SouthernCross.Web.Dto;
 using Xunit;
 
 namespace SouthernCrossWeb.Tests
 {
     public class MemberIdentificationTest
     {
-        [Fact]
-        public void Test1()
-        {
+        private const int DigitLength = 10;
 
+        [Fact]
+        public void MemberCardNumberIdentification_Should_Be_Digits()
+        {
+            var memberIdentification = new MemberIdentification { CardNumber = 1025458014 };
+            Assert.True(memberIdentification.CardNumber.ToString().Length == DigitLength);
+        }
+
+        [Fact]
+        public void PolicyNumberIdentification_Should_Be_Digits()
+        {
+            var memberIdentification = new MemberIdentification { PolicyNumber = 800148901 };
+            Assert.True(memberIdentification.PolicyNumber.ToString().Length == DigitLength);
         }
     }
 }
